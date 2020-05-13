9490e6abaad14d3a4cd61a15f87d82eb3b5b1941
emeka ewelike <emekaewelike@gmail.com>
2020-05-14 00:29:35 +0100

    creating the reg module

--- account/forms.py
+++ account/forms.py
@@ -1,8 +1,21 @@
 from django import forms
+from django.contrib.auth.models import User
 
 #create a form using the forms class
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=25)
     password = forms.CharField(max_length=25, widget=forms.PasswordInput)#the widget allows html traet the password input as a type=password
 
-    
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')# created forms for the fields
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("password don't match")
+        return cd['password2']
