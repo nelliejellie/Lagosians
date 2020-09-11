fcaef888190feb812379909874f6ea69ba95a9aa
emeka ewelike <emekaewelike@gmail.com>
2020-09-11 18:28:42 +0100

    finished index view

--- pages/views.py
+++ pages/views.py
@@ -2,17 +2,24 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from account.models import Profile, Contact
 from images.models import Image
+from account.models import Profile
 from django.contrib.postgres.search import SearchVector
 from .forms import SearchForm
+from Ads.models import Ads
 
 # Create your views here.
 @login_required
 def index_view(request):
     image = Image.objects.order_by('-created')
+    justJoined = Profile.objects.all()[:5]
+    recentAds = Ads.objects.order_by('-created')[:4]
+    # people ff the user
     profile = Contact.objects.filter(user_from=request.user)
     context = {
         'profile' : profile,
         'image' : image,
+        'justJoined': justJoined,
+        'recentAds': recentAds,
     }
     return render(request, 'pages/index.html', context)
 
